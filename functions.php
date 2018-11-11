8c4b047bf930da0325e1165d557a4a8ed25fa11f
124c4a11 <124c4a11@mail.ru>
2018-11-11 06:43:25 +0300

    footer.php

--- functions.php
+++ functions.php
@@ -113,6 +113,16 @@ function solid_widgets_init() {
     'before_title'  => '<h2 class="widget-title">',
     'after_title'   => '</h2>',
   ) );
+
+  register_sidebar( array(
+    'name'          => esc_html__( 'Footer', 'solid' ),
+    'id'            => 'sidebar-footer',
+    'description'   => esc_html__( 'Footer area', 'solid' ),
+    'before_widget' => '<div id="%1$s" class="col-lg-4 %2$s">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h4>',
+    'after_title'   => '</h4> <div class="hline-w"></div>',
+  ) );
 }
 add_action( 'widgets_init', 'solid_widgets_init' );
 
